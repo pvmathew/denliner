fc79af924079c0509c753b4d820b62271a82cafa
Pavin Mathew <pvmathew@ucdavis.edu>
2024-06-05 18:05:13 +0900

    Add nickname field to main menu that auto-saves/loads to config

--- Scenes/MainMenu/main_menu.gd
+++ Scenes/MainMenu/main_menu.gd
@@ -1,5 +1,22 @@
 extends Node2D
 
+const HOST_SCENE_PATH = "res://Scenes/Host/Host.tscn"
+const JOIN_SCENE_PATH = "res://Scenes/Join/Join.tcsn"
+
+var nickname_manager = preload("res://Scenes/MainMenu/nickname_manager.gd").new()
+
+func _ready():
+	var nickname = nickname_manager.load_nickname()
+	$NicknameInput.text = nickname
+
 
 func _on_button_pressed():
 	get_tree().quit()
+
+
+func _on_host_pressed():
+	get_tree().change_scene_to_file(HOST_SCENE_PATH)
+	
+
+func _on_nickname_input_text_changed(new_nickname):
+	nickname_manager.save_nickname(new_nickname)
