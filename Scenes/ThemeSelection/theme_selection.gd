639a80c8534fc56346424f53784fcbc438d55329
Pavin Mathew <pvmathew@ucdavis.edu>
2024-06-16 14:26:18 +0900

    Make HTTP get request on host once a topic is chosen

--- Scenes/ThemeSelection/theme_selection.gd
+++ Scenes/ThemeSelection/theme_selection.gd
@@ -1,7 +1,41 @@
 extends Node2D
 
+var ANIMAL_API_URL = "https://api.github.com/repos/godotengine/godot/releases/latest"
+var http_request = HTTPRequest.new()
+
 func _ready():
+
+	add_child(http_request)
+	http_request.request_completed.connect(_on_request_completed)
+
 	if PlayerData.is_host:
 		$ThemeList.visible = true
 	else:
 		$LobbyDisclaimer.visible = true
+
+
+func _on_どうぶつ_pressed():
+	http_request.request(ANIMAL_API_URL)
+
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	$ThemeList.visible = false
+	
+	var topic = "うさぎ"
+	GameData.topic = topic
+	$Topic.text = topic
+	
+	start_timer()
+
+func start_timer():
+	var timer = Timer.new()
+	timer.wait_time = 2  # Set the wait time to 10 seconds
+	timer.one_shot = true  # Set to true to auto-reset after timeout
+	timer.connect("timeout", _on_timeout)
+	add_child(timer)  # Add the Timer node to the scene tree
+	timer.start()  # Start the timer
+
+func _on_timeout():
+	print("time has run out, scenes to first canvas")
+	# This method is called when the timer expires
+	# Change to your desired scene after 10 seconds
